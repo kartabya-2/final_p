16c6b926055ff30b70e798f0b057bd28d54d737b
kartabya-2 <d7780984@gmail.com>
2024-05-28 23:58:56 +0530

    r_o_i.py

--- r_o_i.py
+++ r_o_i.py
@@ -1,23 +1,36 @@
 from moviepy.editor import VideoFileClip
-
-def max_co():
-  try:
-    clip = VideoFileClip(input("Give me Path"))
-  except FileNotFoundError:
-    print(f"Error: Video file not found.")
-    return None
-
-  max_x,max_y = clip.w - 1,  clip.h - 1  # Adjust for zero-based indexing
-  clip.close()
-  return max_x, max_y
+import cv2
 
 
 
 def main():
   dimen = max_co()
   if dimen is not None:
-    l,h = dimen
+    l,h = dimen[0],dimen[1}
     print(f"So the video has {l}x{h} res")
+    r_o_i(dimen[2])
+    
+
+def max_co():
+  try:
+    clip = VideoFileClip(input("Give me Path"))
+    frame_rate = clip.fps
+    frame_number = int(frame_rate * 3)
+    frame = clip.get_frame(frame_number)
+  except FileNotFoundError:
+    print(f"Error: Video file not found.")
+    return None
+ max_x,max_y = clip.size
+  clip.close()
+  return [max_x-1, max_y-1, frame]# Adjust for zero-based indexing
+  
+def r_O_i(frame):
+  gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+  
+  
+  
+  
 
 
 if __name__ == "__main__":
